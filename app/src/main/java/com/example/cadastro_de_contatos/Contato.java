bb8e043b469f935312783f91499b7f43af7ee1c1
Samuel R. dos Santos <102561330+SamCodeX01@users.noreply.github.com>
2025-05-05 22:58:29 -0300

    Métodos Getters e Setters adicionado na classe Contato

--- app/src/main/java/com/example/cadastro_de_contatos/Contato.java
+++ app/src/main/java/com/example/cadastro_de_contatos/Contato.java
@@ -20,4 +20,34 @@ public class Contato {
         this.celular = celular;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+     public String getNome(){
+        return nome;
+     }
+     public void setNome(String nome){
+        this.nome = nome;
+     }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
